933c3c1e3d93040a2378758cd884e209468982e9
PhantomOz <faniogor@gmail.com>
2020-11-01 14:35:56 +0100

    PhantomOz SideHustle Fronte-End task

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ alert("welcome to PhantomOz Pay Calculator")
 document.querySelector("button").addEventListener("click", function(){
     const x = document.getElementById("hoursworked").value;
     const y = document.getElementById("extra-hours").value;
-    const pay = (x * 1000) + (y * 200);
-    document.querySelector("p").innerHTML = "Your Fee is #" + pay + ".00"
+    if (x > 4){
+        let r = x - 4;  
+        const pay = (4 * 1000) + ( r * 1200) + (y * 1200);
+        document.querySelector("p").innerHTML = "Your Fee is #" + pay + ".00"
+    }else{
+        const pay = (x * 1000) + (y * 1200);
+        document.querySelector("p").innerHTML = "Your Fee is #" + pay + ".00"
+
+    }
+    
 });
